c46f654d136fd9338f0bbb4f7d3253f881745029
Tessa Samuels <tsamu3@illinois.edu>
2022-01-27 02:15:08 -0600

    MP1 completed

--- mp1/prog1.asm
+++ mp1/prog1.asm
@@ -95,19 +95,99 @@ GET_NEXT
 	BRnzp COUNTLOOP		; go to start of counting loop
 
 
+; the program uses a count down from 27 and a count up from 0 to 
+; print all 27 values stored in the histogram.
+; the start address of the histogram is added with the number of lines already printed (R5)
+; then the value at that new address is loaded into the register to be printed.
+; the hex value of @ is also added with the number of lines already printed to get the 
+; character for the histogram value that will print. 
 
+; Registers
+; R0-what will print
+; R1-digit counter
+; R2-value stored
+; R3-bit counter
+; R4-digit
+; R5-offset from start of histagram
+; R6-loop counter of 27
+
+; inital values of the full print_hist loop
 PRINT_HIST
+	LD R6, NUM_BINS		; load #27 into loop counter
+	AND R5,R5,#0		; init R5 to 0
 
-; you will need to insert your code to print the histogram here
+; print the bin label and space and load value to be printed 
+LINE_START		
+	AND R0,R0,#0		; clear R0
+	LD R2, HIST_ADDR	; get start address of histogram
+	ADD R2,R2,R5		; add offset for how many letters have printed
+	LDR R2, R2, #0		; load value at address
+	LD R0, LETTER		; get @
+	ADD R0,R0,R5		; add offset for bin lables
+	OUT			; print bin labels
+	LD R0, SPACE		; load space
+	OUT			; print space
+; set digit counter
+INITS	
+	AND R1,R1,#0		; clear digit counter
+	ADD R1,R1,#4		; set digit counter to 4
 
-; do not forget to write a brief description of the approach/algorithm
-; for your implementation, list registers used in this part of the code,
-; and provide sufficient comments
+; start digit counter loop and set bit counter and digit
+DIGITCOUNTER
+	AND R3,R3,#0		; clear bit counter
+	ADD R3,R3,#4    	; init bit counter
+	AND R4,R4,#0		; set digit to 0
 
+; start bit counter loop, add 1 to digit if value is negative
+LOOP	
+	ADD R4,R4,R4    	; left shift digit
+        ADD R2,R2,#0    	; see if the value is + or -
+        BRzp SKIP       	; if bit is 0 skip adding 1 to digit
+	ADD R4,R4,#1    	; add 1 to digit
 
+; continuation of bit counter loop to skip adding 1 to digit
+SKIP
+        ADD R2,R2,R2    	; left shift R2 to be ready to read next digit
+        ADD R3,R3,#-1   	; decrement bit counter
+        BRp LOOP        	; if there are more bits repeat LOOP
+
+; the 4 bits have been counted now print correct character
+NEGATIVE
+        ADD R0,R4,#-9   	; is digit <= 9?
+        BRnz PRINTZERO		; if digit is <= 9 branch
+        LD R0, A        	; load A ASCII
+        ADD R0,R0,R4		; add digit to A
+        ADD R0,R0,#-10		; subtract 10 
+	BRnzp OUTTIME		; branch to print
+
+; digit >9 so ZERO is initally loaded to R0 to print correct character
+PRINTZERO
+	LD R0, ZERO     	; load 0 ASCII
+	ADD R0,R4,R0		; add digit to 0
+
+; print the character indicated by digit. check if all 4 digits have been printed
+; decrement loop counter and increment offset 
+OUTTIME
+	OUT			; print the group of 4 bits in ASCII 
+	ADD R1,R1,#-1		; decrement digit counter
+	BRp DIGITCOUNTER	; there are more digits to print
+	ADD R6,R6,#-1		; decrement outer loop counter
+	BRnz DONE		; if R6 reaches 0 all lines have printed
+	ADD R5,R5,#1		; increment offset
+
+; all 4 digits have been printed, print a new line and loop to start a new line
+NEW_LINE
+	LD R0, ASCII_NL		; load new line
+	OUT			; print new line
+	BRnzp LINE_START	; all digits have printed.
 
 DONE	HALT			; done
 
+ZERO    	.FILL x30
+A       	.FILL x41
+ASCII_NL	.FILL x0A
+LETTER		.FILL x40
+SPACE		.FILL x20
 
 ; the data needed by the program
 NUM_BINS	.FILL #27	; 27 loop iterations
