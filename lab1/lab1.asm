5c69c10acd5ee10a2d3e2bac69e762770c0c6978
Tessa Samuels <tsamu3@illinois.edu>
2022-02-24 20:21:50 -0600

    mp4 finished

--- lab1/lab1.asm
+++ lab1/lab1.asm
@@ -1,2 +1,53 @@
+
+
+
+; R0-what will print
+; R1-digit counter
+; R2-bit counter
+; R3-value stored
+; R4-digit
+
 .ORIG x3000
 ; write your code here
+
+AND R1,R1,#0	; clear digit counter
+ADD R1,R1,#4	; init digit counter
+AND R4,R4,#0	; clear digit
+AND R2,R2,#0	; clear bit counter
+
+DIGITCOUNTER	ADD R1,R1,#0	; check value of digit counter
+		BRz DONE	; all digits have printed
+		ADD R2,R2,#4	; init bit counter
+		AND R4,R4,#0		
+
+LOOP		ADD R2,R2,#0	
+		BRz NEGATIVE
+		ADD R4,R4,R4	; left shift digit
+		ADD R3,R3,#0	; R3 <0?
+		BRp SKIP 	; skip adding 1 to digit 
+		ADD R4,R4,#1	; add 1 to digit
+SKIP		ADD R3,R3,R3	; left shift R3
+		ADD R2,R2,#-1	; decrement bit counter
+		BRp LOOP	; if there are more bits repeat LOOP
+NEGATIVE 	ADD R0,R4,#-9	; is digit <= 9?
+		BRn PRINTZERO
+		LD R0, A	; load A ASCII
+		ADD R0,R0,R4
+		ADD R0,R0,#-10
+		BRnzp OUTTIME
+		
+PRINTZERO	LD R0, ZERO	; load 0 ASCII
+		ADD R0,R4,R0
+
+OUTTIME		ADD R1,R1,#-1
+		OUT
+		BRnzp DIGITCOUNTER	
+
+ZERO	.FILL x30
+A	.FILL x41
+
+
+DONE	HALT
+
+.END
+
--- mp4/semiprime.c
+++ mp4/semiprime.c
@@ -6,37 +6,52 @@
  * is_prime: determines whether the provided number is prime or not
  * Input    : a number
  * Return   : 0 if the number is not prime, else 1
- */
+
+ The first error was the is_prime had return 0 at the end of the function which gave the program a blank output.
+ The second error was that there was that 1 was being returned when the number was not prime
+
+*/
 int is_prime(int number)
 {
     int i;
     if (number == 1) {return 0;}
     for (i = 2; i < number; i++) { //for each number smaller than it
         if (number % i == 0) { //check if the remainder is 0
-            return 1;
+            return 0;
         }
     }
-    return 0;
+    return 1;
 }
-
 
 /*
  * print_semiprimes: prints all semiprimes in [a,b] (including a, b).
  * Input   : a, b (a should be smaller than or equal to b)
  * Return  : 0 if there is no semiprime in [a,b], else 1
- */
+ 
+ The program was taking the remainder of i/j and seeing if that was prime. since if (i%j == 0) was already in 
+ the program before k = i%j k would always be 0 since is_prime(j) doesnt change the value of j 
+ this issue was fixed by taking is_prime(i/j) instead of is_prime(k)
+ Once that error was fixed the next error was that when a number was semiprime it would print
+ to the screen multiple times. This issue was fixed by using the no longer in use variable k.
+ for each new i value k is set to 0 and once an i value is found to be semiprime the program sets 
+ k to 1 to indicate that the particular i value has already been printed. then k is set to 0 
+ once i moves to the next value
+*/
 int print_semiprimes(int a, int b)
 {
     int i, j, k;
     int ret = 0;
     for (i = a; i <=b; i++) { //for each item in interval
         //check if semiprime
+        k = 0;
         for (j = 2; j <= i; j++) {
             if (i%j == 0) {
                 if (is_prime(j)) {
-                    k = i%j;
-                    if (is_prime(k)) {
-                        printf("%d ", i);
+                    if (is_prime(i/j)) {
+                        if (k == 0) {
+                            printf("%d ", i);
+                            k = 1;
+                        }
                     }
                 }
             }
@@ -45,5 +60,4 @@ int print_semiprimes(int a, int b)
     }
     printf("\n");
     return ret;
-
 }
